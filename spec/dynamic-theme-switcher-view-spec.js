c5183a2a309af347c50536d055afae1a8b49adab
herock <herock@gmail.com>
2018-10-01 08:46:04 +0800

    init commit

---
+++ spec/dynamic-theme-switcher-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DynamicThemeSwitcherView from '../lib/dynamic-theme-switcher-view';
+
+describe('DynamicThemeSwitcherView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
